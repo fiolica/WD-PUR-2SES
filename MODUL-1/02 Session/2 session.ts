b9d1ed58a28ce79735208a13dc3cafe66c444f57
Fiona <fioleeca@gmail.com>
2025-11-16 16:05:12 +0700

    if statement and looping

--- MODUL-1/02 Session/2 session.ts
+++ MODUL-1/02 Session/2 session.ts
@@ -1,3 +1,14 @@
 console.log("Github")
 
 console.log("change")
+
+/**
+ * This function takes a date string in the format "YYYY-MM-DD" and converts it into a string in the format "DD MMM YYYY".
+ * @param {string} date - The date to be converted.
+ * @returns {string} - The converted date string.
+ */
+function convertDate(date: string): string {
+  const newDate = new Date(date);
+  // use toDateString() to convert the date to a string in the format "DD MMM YYYY"
+  return newDate.toDateString();
+}
